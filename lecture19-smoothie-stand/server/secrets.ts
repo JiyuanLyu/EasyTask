961eecfe42ec6a09b111d0915b532fc246c7c7ea
Chelsea Lyu <97059580+JiyuanLyu@users.noreply.github.com>
2024-05-06 18:44:57 -0400

    Delete lecture19-smoothie-stand directory

--- lecture19-smoothie-stand/server/secrets.ts
+++
@@ -1,8 +0,0 @@
-export const gitlab = {
-  // client_id: "ea20d1eb9836441da1e642ae9d435590582660ff1183c275720878c935c228b7",
-  // client_secret:
-  //   "gloas-5364801dc3af2dca14ceb8020c9b967a003800a7cde0bb25ef5f6a215789807e",
-  client_id: "866fde28216f9eb7650a4b52ebf0a0d47ea585d6cd5a94c0e7faca3c5c7dadf7",
-  client_secret:
-    "gloas-8fc08f1d923a5d18c9525abbfbc45be3c4cab7e9b785defa89ba5644a8e518e9",
-}
